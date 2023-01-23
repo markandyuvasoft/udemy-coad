158f79d03a7eeb6a61381912ad2862292597dd1d
markandyuvasoft <markandyuvasoft408@gmail.com>
2023-01-23 05:20:53 -0800

    second

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,32 @@
+const path = require('path')
 const express = require('express')
+
+const rootDir = require('../../express/util/path.js')
 
 const router = express.Router()
 
 
+const products = []
+
+
 router.get('/addProduct',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">send</button></form>')
+    // res.sendFile(path.join(rootDir, 'views', 'add-Product.html'))
+
+    res.render('add-Product', {pageTitle: 'Add Product', path: '/admin/add-Product', formsCSS:true , productCSS:true, activeAddProduct: true})
+   
 })
 
 
-router.post('/product', (req,res,next)=>{
+router.post('/addProduct', (req,res,next)=>{
 
-    console.log(req.body);
+    // console.log(req.body);
+
+    products.push({title: req.body.title})
 
     res.redirect('/')
 })
 
-module.exports =router
+// module.exports =router
+
+exports.routes = router
+exports.products = products
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,24 @@
+const path = require('path')
 const express = require('express')
+
+const rootDir = require('../../express/util/path.js')
+
+const adminData = require('./admin')
+
 
 const router = express.Router()
 
-
-
 router.get('/',(req,res,next)=>{
-    res.send("<h1>hello from express</h1>")
+    // res.send("<h1>hello from express</h1>")    .............normal
+
+    // console.log('shop.js',adminData.products); ..............html
+    // res.sendFile(path.join(rootDir, 'views', 'shop.html'))....html
+
+    const products = adminData.products
+
+    res.render('shop',{prods: products, docTitle:'shop', path:'/', hasProducts:products.length>0, activeShop:true, productCSS:true})
+
+
 })
 
 
